ac0be6e3ce4af3c33d41582553e725313994fe57
jeppeter <jeppeter@gmail.com>
2019-09-10 16:21:19 +0800

    to add modi user mode ps1

---
+++ test/modiusermode.ps1
@@ -0,0 +1,22 @@
+# ParamTest.ps1 - Show some parameter features
+# Param statement must be first non-comment, non-blank line in the script
+Param(
+    $Personal,
+    $Desktop,
+    $Favorites,
+    $MyMusic,
+    $MyPic,
+    $MyVideo,
+    $Downloads,
+    $SavedGames,
+    $Contacts)
+
+
+if (![string]::IsNullOrEmpty($Personal)) {
+    Write-Host "Personal [$Personal]";
+}
+
+
+
+
+
