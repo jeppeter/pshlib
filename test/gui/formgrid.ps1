06cddeea781117b7b892cbae18249e0d64d6e9f9
jeppeter <jeppeter@gmail.com>
2019-09-17 14:57:52 +0800

    to make right and wrong type

--- test/gui/formgrid.ps1
+++ test/gui/formgrid.ps1
@@ -2,6 +2,11 @@
 Add-Type -AssemblyName System.Windows.Forms;
 Add-Type -AssemblyName System.drawing;
 
+function get_file_img($file)
+{
+    $fitem = (Get-Item $file);
+    return [System.Drawing.Image]::Fromfile($fitem);
+}
 
 #Define Form
 $form1 = New-Object System.Windows.Forms.Form
@@ -20,7 +25,7 @@ $DataGridView_ServerName.AllowUserToDeleteRows = $false
 $DataGridView_ServerName.Top = 10
 $DataGridView_ServerName.Left = 10
 $DataGridView_ServerName.Height = 100
-$DataGridView_ServerName.Width = 310
+$DataGridView_ServerName.Width = 410
 $DataGridView_ServerName.ColumnHeadersVisible = $true
 $DataGridView_ServerName.ColumnHeadersHeight = 20 
 $DataGridView_ServerName.Enabled = $true
@@ -29,32 +34,41 @@ $DataGridView_ServerName.Enabled = $true
 $Column1 = New-Object System.Windows.Forms.DataGridViewTextboxColumn
 $Column2 = New-Object System.Windows.Forms.DataGridViewTextboxColumn
 $Column3 = New-Object System.Windows.Forms.DataGridViewCheckBoxColumn;
+$Column4 = New-Object System.Windows.Forms.DataGridViewImageColumn;
 
         
 $Column1.Width = 80
 $Column2.Width = 80
-$Column3.Width = 80
+$Column3.Width = 80;
+$Column4.Width = 80;
 
 $DataGridView_ServerName.ColumnHeadersVisible = $true
 $DataGridView_ServerName.Columns.Add($Column3)
 $DataGridView_ServerName.Columns.Add($Column1);
 $DataGridView_ServerName.Columns.Add($Column2);
+$DataGridView_ServerName.Columns.Add($Column4);
 $DataGridView_ServerName.Columns[0].Name = "select"
 $DataGridView_ServerName.Columns[1].Name = "column1"
 $DataGridView_ServerName.Columns[2].Name = "column2"
+$DataGridView_ServerName.Columns[3].Name = "changed"
 
+
+$wrongimg = get_file_img -file "$pwd\wrong.png";
+$rightimg = get_file_img -file "$pwd\right.png";
 
         
 
 $form1.Controls.Add($DataGridView_ServerName);
 
-#Add Rows
-$DataGridView_ServerName.Rows.Add($false,"1", "2")
-$DataGridView_ServerName.Rows.Add($true,"a", "b")
-
 $DataGridView_ServerName.Columns[0].ReadOnly = $false;
+$DataGridView_ServerName.Columns[3].ReadOnly = $true;
 
-Write-Host "ReadOnly " $DataGridView_ServerName.Columns[0].ReadOnly;
+#Add Rows
+$DataGridView_ServerName.Rows.Add($false,"1", "2", $rightimg);
+$DataGridView_ServerName.Rows.Add($true,"a", "b", $wrongimg);
+$DataGridView_ServerName.Rows[1].Cells[0].ReadOnly = $true;
+
+
 
 
 $form1.ShowDialog() 
